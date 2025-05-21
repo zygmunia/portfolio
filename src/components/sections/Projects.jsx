70b601df32acba340ffd29280f9e14083a6d5d32
Ian Zygmunt <zygmunia@dukes.jmu.edu>
2025-05-21 19:31:33 -0400

    project update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -25,7 +25,7 @@ export const Projects = () => {
                        ))}
                     </div>
                     <div className="flex justify-between items-center">
-                        <a href="https://github.com/zygmunia/django_local_library" className="text-orange-400 hover:text-orange-500 transition-colors my-4">View Project → </a>
+                        <a href="https://www.lithub.live" className="text-orange-400 hover:text-orange-500 transition-colors my-4">View Project → </a>
                     </div>
             </div>
             <div className="p-6 rounded-xl border border-orange-400 hover:-translate-y-1 hover:border-orange-200/30 hover:
@@ -47,28 +47,6 @@ export const Projects = () => {
                     </div>
                     <div className="flex justify-between items-center">
                         <a href="https://github.com/zygmunia/y86-cpu-architecture" className="text-orange-400 hover:text-orange-500 transition-colors my-4">View Project → </a>
-                    </div>
-            </div>
-            <div className="p-6 rounded-xl border border-orange-400 hover:-translate-y-1 hover:border-orange-200/30 hover:
-                    shadow-[0_2px_8px_rgba(255, 105, 36, 0.8)] transition-all">
-                    <h3 className="text-xl font-bold mb-2">DHCP Server and Client Implementation</h3>
-                    <p className="mb-4">
-                    Designed and implemented a fully functional Dynamic Host Configuration Protocol(DHCP) server and client to
-                    facilitate automatic IP address assignment and network configuration. This program focused on network
-                    communication, packed parsing, and protocol compliance.
-                    </p>
-                    <div className="flex flex-wrap gap-2 mb-4">
-                        {["DHCP", "UDP", "C",].map((tech,key)=>(
-                           <span 
-                           key = {key}
-                           className="bg-orange-400/10 text-orange-400 py-1 px-3 rounded-full text-sm hover:bg-orange-600/20
-                           hover:shadow-[0_2px_8px_rgba(255, 105, 36, 0.8)] transition">
-                           {tech}
-                           </span>
-                       ))}
-                    </div>
-                    <div className="flex justify-between items-center">
-                        <a href="https://github.com/zygmunia" className="text-orange-400 hover:text-orange-500 transition-colors my-4">View Project → </a>
                     </div>
             </div>
             </div>
